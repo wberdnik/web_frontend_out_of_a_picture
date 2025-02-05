28eba79ef57ac96927196e49cacf845f3e282e5b
Morph <wberdnik@gmail.com>
2025-02-05 10:46:39 +0300

    demo of vinil

---
+++ picture_records.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// This is demo
+
+module.exports = {
+    mapOfVinil: [
+        {top: 5, height: 3, url: '/var/www/pkg/file.wasm', type: 0,}, // 0 - wasm || code 1770 bytes
+        {top: 8, height: 50, url: 'URL/path/wasm.js', type: 1,}, //68 Кb - 38 lines
+        {top: 60, height: 4, url: 'URL/path/wasm.html', type: 1,}, //4200 - 3 lines
+       /***
+    
+    ]
+};
